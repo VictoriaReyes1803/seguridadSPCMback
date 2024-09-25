27b80205cc4dae4a15b07b0f2e233a0234676258
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-09-25 13:18:58 -0600

    avances

--- users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import User, Reporte, Actividad
-
-admin.site.register(User)
-admin.site.register(Reporte)
-admin.site.register(Actividad)

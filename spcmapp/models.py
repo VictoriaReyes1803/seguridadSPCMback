984180735df902f9348367745efa9e00a171c3b9
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-10-18 09:25:17 -0600

    avances

--- spcmapp/models.py
+++ spcmapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
-
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
